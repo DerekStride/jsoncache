82164be14ee30182d5b9349b6cb87f398de77dc1
DerekStride <djgstride@gmail.com>
2015-12-15 21:08:05 -0500

    Adding RDoc Comments

--- lib/jsoncache.rb
+++ lib/jsoncache.rb
@@ -1,17 +1,39 @@
 require 'json'
 
 # JSONCache is a simple interface to cache JSON based API calls
+#
+# Author::    Derek Stride  (mailto:djgstride@gmail.com)
+# License::   MIT
+#
+# This module provides an easy to use class method +cache+ which caches json
+# in a key-value fashion to the filesystem in /tmp/jsoncache.
 module JSONCache
   extend self
 
   # Retrieves cached data for the specified key and caches the data provided
-  # if the cache isn't valid.
-  # @param [String] key The key in which to check for cached data.
-  # @param [Hash] options A hash of the parameters to use when caching.
+  # if the cache isn't valid. Specify a code block after the method call that
+  # returns a hash and it will be cached.
+  #
+  # ==== Parameters
+  #
+  # +key+:: +String+ The key in which to check for cached data.
+  # +options+:: +Hash+ A hash of the parameters to use when caching.
+  #
+  # ==== Options
+  #
   # Accepted options parameters
-  # => [Boolean] symbolize
-  # => [String] cache_directory
-  # => [Fixnum] delta
+  #
+  # * +:symbolize+:: +Boolean+ Symbolize keys while parsing JSON.
+  # * +:cache_directory+:: +String+ The folder name in /tmp to use as the cache.
+  # * +:delta+:: +Fixnum+ The validity time of the cache in seconds.
+  #
+  # ==== Examples
+  #
+  #   def get_response(uri)
+  #     JSONCache.cache(uri_to_key(uri), delta: 120) do
+  #       query_some_json_api(uri)
+  #     end
+  #   end
   def cache(key, options = {})
     return retrieve_cache(key, options) if cached?(key, options)
     result = yield
@@ -26,8 +48,11 @@ module JSONCache
   ########################################################################
 
   # Determine whether a file is cached and healthy
-  # @param [String] key The key in which to check for cached data.
-  # @param [Hash] options A hash of the parameters to use when caching.
+  #
+  # ==== Parameters
+  #
+  # +key+:: +String+ The key in which to check for cached data.
+  # +options+:: +Hash+ A hash of the parameters to use when caching.
   def cached?(key, options = {})
     timestamp = timestamp_from_key(key, options[:cache_directory])
     delta = options[:delta] || 0
@@ -41,9 +66,12 @@ module JSONCache
   end
 
   # Cache the result from the uri
-  # @param [String] key The key in which to check for cached data.
-  # @param [Hash] data The response to cache.
-  # @param [Hash] options A hash of the parameters to use when caching.
+  #
+  # ==== Parameters
+  #
+  # +key+:: +String+ The key in which to check for cached data.
+  # +data+:: +Hash+ The data to cache.
+  # +options+:: +Hash+ A hash of the parameters to use when caching.
   def cache_file(key, data, options = {})
     return unless data.respond_to?(:to_h)
 
@@ -58,8 +86,11 @@ module JSONCache
   end
 
   # Retrieves a cached value from a key
-  # @param [String] key The key in which to check for cached data.
-  # @param [Hash] options A hash of the parameters to use when caching.
+  #
+  # ==== Parameters
+  #
+  # +key+:: +String+ The key in which to check for cached data.
+  # +options+:: +Hash+ A hash of the parameters to use when caching.
   def retrieve_cache(key, options = {})
     directory = options[:cache_directory]
     filename = filename_from_key(key, directory)
@@ -75,7 +106,10 @@ module JSONCache
   ########################################################################
 
   # Create, if necessary, and return a cache directory
-  # @param [String] directory The name of the cache directory.
+  #
+  # ==== Parameters
+  #
+  # +directory+:: +String+ The name of the cache directory.
   def cache_dir(directory)
     directory ||= 'jsoncache'
     cache_path = File.join('/tmp', directory)
@@ -84,8 +118,11 @@ module JSONCache
   end
 
   # Gets an existing file from a uri if it exists
-  # @param [String] key The key in which to check for cached data.
-  # @param [String] directory The name of the cache directory.
+  #
+  # ==== Parameters
+  #
+  # +key+:: +String+ The key in which to check for cached data.
+  # +directory+:: +String+ The name of the cache directory.
   def filename_from_key(key, directory)
     directory ||= 'jsoncache'
     Dir.foreach(cache_dir(directory)) do |filename|
@@ -95,8 +132,11 @@ module JSONCache
   end
 
   # Extracts a timestamp from an existing file
-  # @param [String] key The key in which to check for cached data.
-  # @param [String] directory The name of the cache directory.
+  #
+  # ==== Parameters
+  #
+  # +key+:: +String+ The key in which to check for cached data.
+  # +directory+:: +String+ The name of the cache directory.
   def timestamp_from_key(key, directory)
     path = filename_from_key(key, directory)
     return 0 if path.nil?
